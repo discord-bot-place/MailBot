fd14241818a5a761c50d7fe9d9eb90350102e663
JockeRider199 <40937306+JockeRider199@users.noreply.github.com>
2018-12-08 18:01:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 //////////////////////////////////////REQUIREMENTS\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 const Discord = require('discord.js');
 const Client = new Discord.Client()
-const login = require('./config.json');
+const settings = require('./config.json')
+
+const login = settings.TOKEN
+const BOT_ID = settings.BOT_ID
+const CHANNEL_ID = settings.CHANNEL_ID
+const ACTIVITY = settings.ACTIVITY
 
 /////////////////////////////////////CONNECTION\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-Client.login(login.token)
+Client.login(login)
 
 Client.on("ready", function(){
-    console.log(`Connecté en temps que ${Client.user.tag} sur ${Client.guilds.size} serveurs`)
-    Client.user.setActivity(`Envoie moi un message pour contacter le staff`)
+    console.log(`Connecté en temps que ${Client.user.tag}\n`)
+    Client.user.setActivity(ACTIVITY)
 })
 
 ///////////////////////////////////DMs FUNCTION\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 Client.on('message', message => {
-    if(message.channel.type === "dm"){//Only Works if it's a private message
+    if(message.channel.type === "dm"){
         var args = message.content.split(" ").slice(0).join(" ")
-        var ChannelToSend = Client.channels.get("CHANNEL_ID")//where you want the reports to arrive
-        var BotID = "YOUR_BOT_ID"
+        var ChannelToSend = Client.channels.get(CHANNEL_ID)
 
         if(args.length > 256) return message.channel.send("Ton message contient trop de caractères...")
-        if(message.author.id === BotID) return;
+        if(message.author.id === BOT_ID) return;
         var Embed = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
         .setColor("#5cbdea")
         .setTitle("Nouveau message privé !")
         .addField("Message de :", message.author.tag + "\n ID : " + message.author.id)
         .addField("Contenu du message :", args)
+        .setTimestamp()
         ChannelToSend.send(Embed)
         message.channel.send("Le message est bien passé ! :incoming_envelope:")
+        console.log(`Nouveau message de : ${message.author.tag} \n --> ${args}`)
     }
 })
---
+++ launch.bat
@@ -0,0 +1,3 @@
+@echo off
+title MailBot
+node index.js
